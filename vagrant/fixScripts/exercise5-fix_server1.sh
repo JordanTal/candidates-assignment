fe38b28c681f793fc138cf54c9a3f1d4ccc52748
Yarden Tal <yardent@algotec.co.il>
2020-02-26 17:03:44 +0200

    commit of solutions - missing exercise3 ,and exercise 5 on server 2.

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
+ssh-keygen -f ~/.ssh/id_rsa -t rsa -N ''
+sudo apt-get install sshpass
+sshpass -p "vagrant" ssh-copy-id -i ~/.ssh/id_rsa.pub vagrant@server2
+sudo sed -i -e "\$aStrictHostKeyChecking no" /etc/ssh/ssh_config
--- vagrant/fixScripts/exercise6-fix.sh
+++ vagrant/fixScripts/exercise6-fix.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
 #add fix to exercise6-fix here
+
+sudo apt-get install sshpass
+last = ${@:$#}
+FILES_TOTAL_SIZE=0
+host=$(hostname)
+
+if ["$os" = "server1" ] ; then
+    dest="server2"
+else
+    dest="server1"
+fi
+
+for FILE in "${@:1:$#-1}"; do
+	sshpass -p "vagrant" scp $FILE vagrant@"$dest":"$last"
+	FILES_TOTAL_SIZE+=$(stat -c%s "$FILE")
+done
+
+
