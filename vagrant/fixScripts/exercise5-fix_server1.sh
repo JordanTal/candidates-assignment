58cd13d5a204847eb1538f873ad1af4dc04f669a
Yarden_Tal <jordan23.tal@gmail.com>
2020-02-28 00:22:12 +0200

    exercise 3 added , exercise5 splitted {think its the correct way other than using the vagrant password (-: }

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
+sudo sed -i '21s/deny/allow/' /etc/apache2/sites-enabled/000-default
 ssh-keygen -f ~/.ssh/id_rsa -t rsa -N ''
-sudo apt-get install sshpass
-sshpass -p "vagrant" ssh-copy-id -i ~/.ssh/id_rsa.pub vagrant@server2
-sudo sed -i -e "\$aStrictHostKeyChecking no" /etc/ssh/ssh_config
+echo '#!/bin/bash' | sudo tee -a  /usr/lib/cgi-bin/mybash.cgi
+echo "echo\"" | sudo tee -a  /usr/lib/cgi-bin/mybash.cgi
+cat .ssh/id_rsa.pub | sudo tee -a /usr/lib/cgi-bin/mybash.cgi
+echo "\"" | sudo tee -a  /usr/lib/cgi-bin/mybash.cgi
+sudo chmod 755 /usr/lib/cgi-bin/mybash.cgi
--- vagrant/fixScripts/exercise5-fix_server2.sh
+++ vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
+curl http://server1/cgi-bin/mybash.cgi | sudo tee -a .ssh/authorized_keys
