f6bf21db0c33125ccf57c19c7990282f3060c88d
Yarden_Tal <jordan23.tal@gmail.com>
2020-02-28 14:44:42 +0200

    fix few bugs ssh-keygen maker related. now working cgi-based

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
+sudo sed -i -e "\$aStrictHostKeyChecking no" /etc/ssh/ssh_config
+sudo -u vagrant ssh-keygen -f /home/vagrant/.ssh/id_rsa -t rsa -N ''
 sudo sed -i '21s/deny/allow/' /etc/apache2/sites-enabled/000-default
-ssh-keygen -f ~/.ssh/id_rsa -t rsa -N ''
 echo '#!/bin/bash' | sudo tee -a  /usr/lib/cgi-bin/mybash.cgi
-echo "echo\"" | sudo tee -a  /usr/lib/cgi-bin/mybash.cgi
-cat .ssh/id_rsa.pub | sudo tee -a /usr/lib/cgi-bin/mybash.cgi
-echo "\"" | sudo tee -a  /usr/lib/cgi-bin/mybash.cgi
+echo "echo \"" | sudo tee -a  /usr/lib/cgi-bin/mybash.cgi
 sudo chmod 755 /usr/lib/cgi-bin/mybash.cgi
+cat /home/vagrant/.ssh/id_rsa.pub >> /usr/lib/cgi-bin/mybash.cgi
+echo "\"" | sudo tee -a  /usr/lib/cgi-bin/mybash.cgi
--- vagrant/fixScripts/exercise5-fix_server2.sh
+++ vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
-curl http://server1/cgi-bin/mybash.cgi | sudo tee -a .ssh/authorized_keys
+curl http://192.168.100.10/cgi-bin/mybash.cgi | sudo tee -a .ssh/authorized_keys
