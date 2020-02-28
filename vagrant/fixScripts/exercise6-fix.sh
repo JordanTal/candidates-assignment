51f32bd1fa5fb54ad41d10914de3ecc9da72a3b5
Yarden_Tal <jordan23.tal@gmail.com>
2020-02-28 19:29:59 +0200

    Update exercise6 + format the file using dos2unix (syntax issues)

--- vagrant/fixScripts/exercise6-fix.sh
+++ vagrant/fixScripts/exercise6-fix.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 #add fix to exercise6-fix here
-
 sudo apt-get install sshpass
-last = ${@:$#}
+destFolder=${@:$#}
 FILES_TOTAL_SIZE=0
+fileSize=0
 host=$(hostname)
-
-if ["$os" = "server1" ] ; then
-    dest="server2"
+touch /home/vagrant$DESTI_FOLDER
+if [ "$host" = "server1" ] ; then
+remoteHost='server2'
 else
-    dest="server1"
+remoteHost='server1'
 fi
-
 for FILE in "${@:1:$#-1}"; do
-	sshpass -p "vagrant" scp $FILE vagrant@"$dest":"$last"
-	FILES_TOTAL_SIZE+=$(stat -c%s "$FILE")
+	sshpass -p "vagrant" scp $FILE vagrant@"$remoteHost":"$destFolder"
+	fileSize=$(stat -c %s "$FILE")
+	FILES_TOTAL_SIZE=$((FILES_TOTAL_SIZE + fileSize))
 done
-
-
+echo "$FILES_TOTAL_SIZE"
